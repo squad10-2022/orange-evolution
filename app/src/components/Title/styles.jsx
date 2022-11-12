e8170445784419fcaa48e849c1ee8ef638b6516e
ericaselen <erica.elen@live.com>
2022-11-12 02:19:33 -0300

    :lipstick: feat: estilização da tela home e estruturação da tela de níveis
    :wq

--- app/src/components/Title/styles.jsx
+++ app/src/components/Title/styles.jsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const H2 = styled.h2`
   color: ${({ color, theme }) => color || theme.primary};
-  font-size: 32px;
+  font-size: ${({ size }) => (size === "md" ? "24px" : "32px")};
   font-weight: 700;
 `;
