fa48caa0603b6cabfcea76c223ee44dd98faf150
Breno Felix <brenodev.felix@gmail.com>
2022-11-13 19:11:14 -0300

    Error

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import LevelsController from "../app/controllers/LevelsController";
 import SessionController from "../app/controllers/SessionController";
+import TrailsController from "../app/controllers/TrailsController";
 import UsersController from "../app/controllers/UsersController";
 
 const routes = new Router();
@@ -7,5 +9,12 @@ const routes = new Router();
 routes.post("/users", UsersController.store);
 
 routes.post("/sessions", SessionController.store);
+
+routes.post("/trails", TrailsController.store);
+routes.get("/trails", TrailsController.index);
+routes.get("/trails/:id", TrailsController.show);
+
+routes.post("/trails/:id/levels", LevelsController.store);
+routes.get("/trails/:id/levels", LevelsController.index);
 
 export default routes;
