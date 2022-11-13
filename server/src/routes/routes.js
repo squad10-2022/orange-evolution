2b3a03caa1ea0f2ba181c617378f5644bc3a76d0
Breno Felix <brenodev.felix@gmail.com>
2022-11-13 01:47:51 -0300

    :sparkles: feat: Rota de Login e Hash de senha com bcrypt

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,34 +1,11 @@
 import { Router } from "express";
+import SessionController from "../app/controllers/SessionController";
 import UsersController from "../app/controllers/UsersController";
-import { Users } from "../app/models/Users";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.status(200).json({ msg: "Teste ok!" });
-});
-
-// exemplo de rotas conectadas com mongoDB
-routes.get("/users", async (req, res) => {
-  const users = await Users.find({});
-  try {
-    res.status(200).send(users);
-  } catch (err) {
-    res.status(500).send({ error: err });
-  }
-});
-
-// routes.post("/users", async (req, res) => {
-//   const user = new Users(req.body);
-//   try {
-//     await user.save();
-//     res.status(201).send(user);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send(error);
-//   }
-// });
-
 routes.post("/users", UsersController.store);
+
+routes.post("/sessions", SessionController.store);
 
 export default routes;
